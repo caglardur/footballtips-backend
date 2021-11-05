6028eed704abfe7a8b88fc0dc51b706c84be4cb0
caglar <cagi666@gmail.com>
2021-11-05 07:11:08 +0300

    index.js add error and router path

--- index.js
+++ index.js
@@ -2,12 +2,31 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
 
-app.use = express()
+const matchRouter = require("./router/match-router")
+const HttpError = require("./models/http.error")
+
+app = express()
 
 app.use(cors())
 app.use(express.json())
 
+app.use("/api", matchRouter)
+
+app.use((req, res, next) => {
+  const error = new HttpError("wrong path", 404)
+  throw error
+})
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error)
+  }
+  res.status(error.code || 500)
+  res.json({ message: error.message || "unknown error" })
+})
+
 mongoose
-  .connect()
-  .then()
+  .connect(process.env.DB_LINK, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(5090))
+  .then(() => console.log("server start 5090"))
   .catch(err => console.log(err))

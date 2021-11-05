7b82fe5511b1e828d7be4c1c70ed73890c9a42d4
caglar <cagi666@gmail.com>
2021-11-05 07:38:32 +0300

    get match by date controller add

--- controller/match-controller.js
+++ controller/match-controller.js
@@ -21,4 +21,25 @@ const getSingleMatch = async (req, res, next) => {
   res.json({ singleMatch })
 }
 
+const getMatchesByDate = async (req, res, next) => {
+  const dateStart = new Date(req.params.date)
+  const dateEnd = new Date(dateStart)
+  dateEnd.setDate(dateEnd.getDate() + 1)
+
+  let matches
+  try {
+    matches = await Match.find({ $and: [{ "fixture.date": { $gte: dateStart } }, { "fixture.date": { $lt: dateEnd } }] })
+  } catch (err) {
+    const error = new HttpError("Something went wrong, please try again", 500)
+    return next(error)
+  }
+  if (!matches || matches.length == 0) {
+    const error = new HttpError("Matches not found", 404)
+    return next(error)
+  }
+
+  res.json({ matches })
+}
+
 exports.getSingleMatch = getSingleMatch
+exports.getMatchesByDate = getMatchesByDate
--- router/match-router.js
+++ router/match-router.js
@@ -5,5 +5,6 @@ const matchController = require("../controller/match-controller")
 const router = express.Router()
 
 router.get("/singleMatch/:matchId", matchController.getSingleMatch)
+router.get("/matchesByDate/:date", matchController.getMatchesByDate)
 
 module.exports = router

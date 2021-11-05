857aa1b074bc6348e88bf6c9582386018b85879c
caglar <cagi666@gmail.com>
2021-11-05 09:15:16 +0300

    update controller and router for search by team id

--- controller/match-controller.js
+++ controller/match-controller.js
@@ -41,5 +41,26 @@ const getMatchesByDate = async (req, res, next) => {
   res.json({ matches })
 }
 
+const getMatchesByTeams = async (req, res, next) => {
+  const { homeTeamId, awayTeamId } = req.params
+
+  let lastMatches
+
+  try {
+    lastMatches = await Match.find({ $and: [{ $or: [{ $or: [{ "teams.home.id": homeTeamId }, { "teams.home.id": awayTeamId }] }, { $or: [{ "teams.away.id": homeTeamId }, { "teams.away.id": awayTeamId }] }] }, { "fixture.status.short": "FT" }] })
+  } catch (err) {
+    const error = new HttpError("Something went wrong, please try again", 500)
+    return next(error)
+  }
+
+  if (!lastMatches || lastMatches.length == 0) {
+    const error = new HttpError("Matches not found", 404)
+    return next(error)
+  }
+
+  res.json({ lastMatches })
+}
+
 exports.getSingleMatch = getSingleMatch
 exports.getMatchesByDate = getMatchesByDate
+exports.getMatchesByTeams = getMatchesByTeams
--- router/match-router.js
+++ router/match-router.js
@@ -6,5 +6,6 @@ const router = express.Router()
 
 router.get("/singleMatch/:matchId", matchController.getSingleMatch)
 router.get("/matchesByDate/:date", matchController.getMatchesByDate)
+router.get("/matchesByTeams/:homeTeamId/:awayTeamId", matchController.getMatchesByTeams)
 
 module.exports = router
